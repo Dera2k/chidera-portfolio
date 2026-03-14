b142dd516dff7cfe7a53e0331fa1f4b268405f45
Nwogu Chidera <chideranwogu2003@gmail.com>
2026-03-14 16:19:17 +0100

    initial commit

---
+++ src/pages/Index.tsx
@@ -0,0 +1,31 @@
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import TechStackSection from "@/components/TechStackSection";
+import ProjectsSection from "@/components/ProjectsSection";
+import ArchitectureSection from "@/components/ArchitectureSection";
+import CurrentlyBuildingSection from "@/components/CurrentlyBuildingSection";
+import ExperienceSection from "@/components/ExperienceSection";
+import PhilosophySection from "@/components/PhilosophySection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <HeroSection />
+      <ProjectsSection />
+      <AboutSection />
+      <TechStackSection />
+      <ArchitectureSection />
+      <CurrentlyBuildingSection />
+      <ExperienceSection />
+      <PhilosophySection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
